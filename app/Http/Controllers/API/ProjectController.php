369c40af27b480f2e722e0fd89a41e6770a41cf1
Piotr Jakowicki <jakowicki.piotr@gmail.com>
2020-10-25 20:22:02 +0100

    Add project store method

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -2,10 +2,112 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Exceptions\InvalidInputException;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use App\Models\Reward;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProjectController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (empty($request->body)) throw new InvalidInputException();
+
+        $data = json_decode($request->body, true);
+
+        $rules = [
+            'id' => 'integer|unique:projects',
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'status' => 'string|in:started,finished,draft',
+        ];
+
+        if (!empty($data['rewards'])) {
+            $rewardsRules = [
+                'rewards.*.id' => 'integer|unique:rewards',
+                'rewards.*.projectId' => 'required|integer|same:id',
+                'rewards.*.name' => 'required|string',
+                'rewards.*.description' => 'required|string',
+                'rewards.*.amount' => 'required|regex:/^\d+(\.\d{1,2})?$/'
+            ];
+
+            $rules = array_merge($rules, $rewardsRules);
+        }
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            throw new InvalidInputException();
+        }
+
+        $project = Project::create($data);
+
+        if (!empty($data['rewards'])) {
+            foreach ($data['rewards'] as $reward) {
+                Reward::create($reward);
+            }
+        }
+
+        return (new ProjectResource($project));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \App\Http\Resources\ProjectResource
+     */
+    public function updateObject(Request $request)
+    {
+    }
+
+    /**
+     * Find the specified resource by status.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return App\Http\Resources\ProjectResource
+     */
+    public function findByStatus(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \App\Http\Resources\ProjectResource
+     */
+    public function show(Project $project)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Project  $project
+     * @return \App\Http\Resources\ProjectResource
+     */
+    public function update(Request $request, Project $project)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+    }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,5 +9,22 @@ class Project extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'status',
+        'description'
+    ];
+
     public $timestamps = false;
+
+    public function rewards()
+    {
+        return $this->hasMany(Reward::class, 'projectId', 'id');
+    }
 }
